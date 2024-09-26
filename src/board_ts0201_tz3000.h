04ebbb7163ecd1068f9dc6fc2a732cfbdfd69507
KipK <hotlobster75@gmail.com>
2024-09-26 14:16:44 +0200

    ported to TS0201_3000bguser20

--- src/board_ts0201_tz3000.h
+++ src/board_ts0201_tz3000.h
@@ -17,17 +17,18 @@
 // TLSR825x 1M Flash
 // GPIO_PA7 - SWS, free, (debug TX)
 // GPIO_PB4 - LED (to GND)
-// GPIO_PC0 - KEY (to GND)
+///// GPIO_PC0 - KEY (to GND)
+// GPIO_PB5 - KEY (to GND)
 // GPIO_PC2 - SDA, used I2C
 // GPIO_PC3 - SCL, used I2C
 // GPIO_PD7 - ALERT (CHT8305)
 
 // BUTTON
-#define BUTTON1             GPIO_PC0
-#define PC0_FUNC			AS_GPIO
-#define PC0_OUTPUT_ENABLE	0
-#define PC0_INPUT_ENABLE	1
-#define	PULL_WAKEUP_SRC_PC0	PM_PIN_PULLUP_10K
+#define BUTTON1             GPIO_PB5
+#define PB5_FUNC			AS_GPIO
+#define PB5_OUTPUT_ENABLE	0
+#define PB5_INPUT_ENABLE	1
+#define	PULL_WAKEUP_SRC_PB5	PM_PIN_PULLUP_10K
 
 // I2C
 #define I2C_CLOCK	400000 // Hz
@@ -46,11 +47,11 @@
 // LED
 #define LED_ON				1
 #define LED_OFF				0
-#define GPIO_LED			GPIO_PB4
-#define PB4_FUNC	  		AS_GPIO
-#define PB4_OUTPUT_ENABLE	1
-#define PB4_INPUT_ENABLE	1
-#define PB4_DATA_OUT		LED_OFF
+#define GPIO_LED			GPIO_PD2
+#define PD2_FUNC	  		AS_GPIO
+#define PD2_OUTPUT_ENABLE	1
+#define PD2_INPUT_ENABLE	1
+#define PD2_DATA_OUT		LED_OFF
 
 // VBAT
 #define SHL_ADC_VBAT		C5P // see in adc.h ADC_InputPchTypeDef
