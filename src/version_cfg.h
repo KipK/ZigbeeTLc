e43862b4a152e7c7406505448bbab7109d148307
KipK <hotlobster75@gmail.com>
2025-04-09 09:48:15 +0200

    fix config for bguser20

--- src/version_cfg.h
+++ src/version_cfg.h
@@ -91,7 +91,7 @@
 
 /* Board define */
 #ifndef BOARD
-#define BOARD					BOARD_MJWSD06MMC // BOARD_LYWSD03MMC or BOARD_CGDK2 or ...
+#define BOARD					BOARD_TS0201_TZ3000 // BOARD_LYWSD03MMC or BOARD_CGDK2 or ...
 #endif
 
 /* Chip IDs */
